7747bf4a8b2e41e80c482837089143914c9cd50e
e-halinen <54105602+e-halinen@users.noreply.github.com>
2023-09-28 13:26:14 +0300

    New prod release: MM-511, MM-523, Light_rail (#111)
    * Add pikaratikka to the maps
    
    * Add icons and colors for pikaratikka
    
    * Fix typo on color code
    
    * Change pikaratikka to updated color
    
    * MM-523: Fix rendering error about minimum radius on circle vectors (#109)
    
    ---------
    
    Co-authored-by: Juho Hänninen <juho.hanninen@cgi.com>

--- src/components/map/stopSymbol.js
+++ src/components/map/stopSymbol.js
@@ -5,14 +5,17 @@ import { colorsByMode } from 'util/domain';
 
 const strokeWidth = 1;
 
-const StopSymbol = props => {
+const StopSymbol = (props) => {
   const modes = [...new Set(props.routes.map(({ mode }) => mode))];
   const colors = [];
-  if (props.routes.some(route => route.trunkRoute)) {
+  if (props.routes.some((route) => route.trunkRoute)) {
     colors.push(colorsByMode.TRUNK);
   }
   if (modes.includes('TRAM')) {
     colors.push(colorsByMode.TRAM);
+  }
+  if (modes.includes('L_RAIL')) {
+    colors.push(colorsByMode.L_RAIL);
   }
   if (modes.includes('RAIL')) {
     colors.push(colorsByMode.RAIL);
@@ -23,7 +26,7 @@ const StopSymbol = props => {
   if (modes.includes('FERRY')) {
     colors.push(colorsByMode.FERRY);
   }
-  if (modes.includes('BUS') && props.routes.some(route => !route.trunkRoute)) {
+  if (modes.includes('BUS') && props.routes.some((route) => !route.trunkRoute)) {
     colors.push(colorsByMode.BUS);
   }
 
@@ -35,13 +38,18 @@ const StopSymbol = props => {
 
   return (
     <svg width={props.size} height={props.size} style={{ display: 'block' }}>
-      <circle cx={props.size / 2} cy={props.size / 2} r={outlines[0].radius} fill="#fff" />
+      <circle
+        cx={props.size / 2}
+        cy={props.size / 2}
+        r={outlines[0].radius !== -1 ? outlines[0].radius : 2.5}
+        fill="#fff"
+      />
       {outlines.map(({ radius, color }, index) => (
         <circle
           key={index}
           cx={props.size / 2}
           cy={props.size / 2}
-          r={radius}
+          r={radius !== -1 ? radius : 2.5}
           stroke={color}
           strokeWidth={strokeWidth}
           fill="none"
