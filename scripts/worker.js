9c1dab07378e06c8871838ad0e28fc4d24a0d6d6
e-halinen <54105602+e-halinen@users.noreply.github.com>
2023-10-19 17:26:05 +0300

    Increase timeout on PDF generation (#113)

--- scripts/worker.js
+++ scripts/worker.js
@@ -86,7 +86,7 @@ async function renderComponent(options) {
     height: height * SCALE,
     pageRanges: '1',
     scale: SCALE,
-    timeout: 60000,
+    timeout: 5 * 60000,
   };
 
   const contents = await page.pdf(printOptions);
